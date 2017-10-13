8a70b97813a7f5faf96f06e0160a32293238d64d
Pradeepibz <pradeep.loganathan@itbeezone.in>
2017-10-13 18:17:03 +0530

    embed css

--- public/embed.js
+++ public/embed.js
@@ -6,8 +6,10 @@ window.onload = function() {
    //iFrame
    var iframe = document.createElement('iframe');
    iframe.style.display = "block";
-   iframe.style.width = "100%";
+   iframe.style.width = "690px";
    iframe.style.height = "400px";
+   iframe.style.margin = "auto";
+   iframe.style["box-shadow"] = "1px 0px 16px 3px #888888";
    iframe.src = "http://localhost:3000/"
    document.body.appendChild(iframe);
 };
