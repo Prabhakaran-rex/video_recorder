88a46706c8f958fde03be97dd472ac6db83c189a
Pradeepibz <pradeep.loganathan@itbeezone.in>
2017-10-16 18:54:29 +0530

    popup video

--- public/embed.js
+++ public/embed.js
@@ -3,13 +3,17 @@ window.onload = function() {
    var scriptPram = document.getElementById('load_widget');
    var id = scriptPram.getAttribute('data-page');
 
+
+
    //iFrame
    var iframe = document.createElement('iframe');
    iframe.style.display = "block";
-   iframe.style.width = "690px";
-   iframe.style.height = "400px";
+   iframe.style.width = "100%";
+   iframe.style.height = "678px";
    iframe.style.margin = "auto";
-   iframe.style["box-shadow"] = "1px 0px 16px 3px #888888";
+   // iframe.style["box-shadow"] = "1px 0px 16px 3px #888888";
    iframe.src = "http://localhost:3000/"
-   document.body.appendChild(iframe);
+   $('body').append("<button type='button' class='btn btn-danger btn-sm' data-toggle='modal' data-target='#videoModal'>Record Video</button> <div id='videoModal' class='modal fade' role='dialog'> <div class='modal-dialog'> <div class='modal-content'> <div class='modal-header'> <button type='button' class='close' data-dismiss='modal'>&times;</button> <h4 class='modal-title'>Video Recorder</h4> </div> <div class='modal-body video-body'></div> <div class='modal-footer'> <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button></div></div></div></div>")
+   $('.video-body').append(iframe);
+   $('.modal-content').css('width','740px');
 };
